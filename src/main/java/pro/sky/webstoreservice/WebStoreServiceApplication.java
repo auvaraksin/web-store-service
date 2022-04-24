8be49b730943985585a4a15eca40f54278ef5ac3
auvararaksin <auvaraksin@mail.ru>
2022-04-24 17:15:49 +0200

    Initial commit

---
+++ src/main/java/pro/sky/webstoreservice/WebStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.webstoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoreServiceApplication.class, args);
+    }
+
+}
